0f4bd59db3d1d544c56408cba358b10e1c73fd4a
ItsRicmor <ricardojms1999@gmail.com>
2019-09-30 10:05:21 -0600

    Delete comments on PrivateRoute

--- src/routes/routes_auth/PrivateRoute.tsx
+++ src/routes/routes_auth/PrivateRoute.tsx
@@ -9,22 +9,4 @@ const PrivateRoute: FC<any> = ({ component: Component, state, ...rest }) => (
     )} />
 )
 
-
-
-    // return <Route {...rest} render={(props) => <StreamBuilder
-    //         stream={authBloc.sesionStateStream()}
-    //         builder={(snapshot: Snapshot<Boolean>) => {
-    //                 const state: Boolean | undefined = snapshot.data;
-    //                 if (state != undefined) {
-    //                     return state
-    //                         ? <Component {...props} />
-    //                         : <Redirect to='/login' />
-    //                 }
-    //                 return <Redirect to='/login' />
-    //             }
-    //         } />
-    //     }
-    // />
-
-
 export default PrivateRoute;
