165de6a6ee5baa613621edb412396d8cd9b9a490
unknown <enrique_ceas@hotmail.com>
2019-10-06 18:38:25 -0600

    Main menu with a little card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,37 +1,108 @@
-import React, { useContext } from 'react' 
-import { Todo } from '../Models/Todo';
-import { BlocsContext } from '../store/Context';
-import { StreamBuilder, Snapshot } from '../utils/BlocBuilder';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Cards from '../components/container/Cards/components/Cards'
 
-interface FromProps {
-    title: string,
+const cards = [1, 2, 3, 4, 5, 6, 7, 8]
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      width: 'auto',
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  }),
+);
 
-}
+export function HomePage() {
+  const classes = useStyles();
 
-const HomePage = ({ title }: FromProps) => {
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            RARIN TECHNOLOGIES
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Buscar…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Cards
 
-    const { todoBloc } = useContext(BlocsContext);
-    todoBloc.loadTodos()
-    return (
-        <div>
-            <h1>{title}</h1>
-            <button onClick={() => todoBloc.addTodo()} >Nuevo</button>
-            <StreamBuilder
-                stream = { todoBloc.todoStrem() }
-                builder = {(snapshot: Snapshot<Todo[]>)=> {
-                    if(snapshot.hasError){
-                        alert(snapshot.error);
-                    }
-                    if(snapshot.hasData){
-                        let todos: Todo[] = snapshot.data || [];
-                        return todos.map((todo, i) => <li key={i} >{todo.title}</li>)
-                    }
-                    return "Loading...";
-                }}
-            /> 
-        </div>
-    );
-
+/>    </div>
+  );
 }
 
 export default HomePage;
