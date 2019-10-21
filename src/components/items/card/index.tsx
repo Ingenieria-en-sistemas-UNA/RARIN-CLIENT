7467daf24bf1991f2b4ddf5658897bf908acac6a
ItsRicmor <ricardojms1999@gmail.com>
2019-10-21 08:51:35 -0600

    Product, Category form

--- src/components/items/card/index.tsx
+++ src/components/items/card/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Card as CardMaterial } from '@material-ui/core';
+import React, { useState } from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Card as CardMaterial, Collapse } from '@material-ui/core';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,22 +8,38 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import programmer from '../../../assets/image/programmer.jpeg';
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-    marginTop: 20
-  },
-  media: {
-    height: 140,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 190,
+      marginTop: 20
+    },
+    media: {
+      height: 120,
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+  }));
 
 const Card = () => {
   const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <CardMaterial className={classes.card}>
-      <CardActionArea>
+      <CardActionArea onClick={handleExpandClick}>
         <CardMedia
           className={classes.media}
           image={programmer}
@@ -46,6 +62,15 @@ const Card = () => {
           Learn More
         </Button>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+        </CardContent>
+      </Collapse>
     </CardMaterial>
 
   );
